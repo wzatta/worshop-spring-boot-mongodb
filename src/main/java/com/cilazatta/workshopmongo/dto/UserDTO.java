d544902729d32a4d9a6acaeeef8d20c4069812fa
wzatta <wzjssa@gmail.com>
2023-01-16 11:14:04 -0300

    Usando padrão DTO para retornar usuarios

---
+++ src/main/java/com/cilazatta/workshopmongo/dto/UserDTO.java
@@ -0,0 +1,50 @@
+package com.cilazatta.workshopmongo.dto;
+
+import java.io.Serializable;
+
+import com.cilazatta.workshopmongo.domain.User;
+
+public class UserDTO implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	private String _id;
+	private String name;
+	private String email;
+	
+	public UserDTO() {
+	}
+
+	public UserDTO(User obj) {
+		_id = obj.get_id();
+		name = obj.getName();
+		email = obj.getEmail();
+	}
+
+	public String get_id() {
+		return _id;
+	}
+
+	public void set_id(String _id) {
+		this._id = _id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+}
--- src/main/java/com/cilazatta/workshopmongo/resources/UserResource.java
+++ src/main/java/com/cilazatta/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.cilazatta.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cilazatta.workshopmongo.domain.User;
+import com.cilazatta.workshopmongo.dto.UserDTO;
 import com.cilazatta.workshopmongo.services.UserService;
 
 @RestController
@@ -19,10 +21,10 @@ public class UserResource {
 	private UserService service;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> list = service.findAll();
-		
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(w->new UserDTO(w)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 		
 	}
 	
