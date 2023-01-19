f863bf50c0240b416e58b613cecc35f2fa460acc
wzatta <wzjssa@gmail.com>
2023-01-19 11:13:02 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/cilazatta/workshopmongo/config/Instantatiation.java
+++ src/main/java/com/cilazatta/workshopmongo/config/Instantatiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import com.cilazatta.workshopmongo.domain.Post;
 import com.cilazatta.workshopmongo.domain.User;
 import com.cilazatta.workshopmongo.dto.AuthorDTO;
+import com.cilazatta.workshopmongo.dto.CommentDTO;
 import com.cilazatta.workshopmongo.repository.PostRepository;
 import com.cilazatta.workshopmongo.repository.UserRepository;
 
@@ -39,6 +40,13 @@ public class Instantatiation implements CommandLineRunner {
 		
 		Post post1 = new Post(null, Instant.now(),"Partiu viagem","Vou viajar para São Paulo. Abraços!",new AuthorDTO(maria));
 		Post post2 = new Post(null, Instant.now(),"Bom Dia", "Acordei feliz hoje!",new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", Instant.now(), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite!!", Instant.now(), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", Instant.now(), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1,c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
